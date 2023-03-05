acadf63dec8447e5e32d659cc1f474fcac930beb
Herdiyan_Adam_Putra <herdiyanadam256@gmail.com>
2023-03-05 16:44:00 +0700

    release production

---
+++ app/Views/crud/home.php
@@ -0,0 +1,20 @@
+<div class="card card-outline card-primary rounded-0 mt-5">
+    <div class="card-body">
+        <div class="container-fluid">
+            <h1 class="text-center" style="font-family:Georgia, 'Times New Roman', Times, serif">Submit Assignment UAS - Programming III </h1>
+            <img src="https://blog.akai.org.pl/media/codeigniter.png" width="15%" height="15%" style="" alt="">
+
+
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/27/PHP-logo.svg/1200px-PHP-logo.svg.png" width="20%" height="15%" alt="">
+
+
+            <img src="https://upload.wikimedia.org/wikipedia/commons/8/87/Sql_data_base_with_logo.png" width="25%" height="20%" alt="">
+
+            <img src="https://i.pinimg.com/originals/5c/3d/4b/5c3d4b018e0995b97171a35e06da9dae.png" width="25%" height="15%" alt="">
+
+
+
+
+        </div>
+    </div>
+</div>
---
+++ app/Views/templates/footer.php
@@ -0,0 +1,19 @@
+        </div>
+        <!-- Main Container -->
+        </div>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+
+        <!--End of Wrapper -->
+        <footer style="text-align:center" ;><b>Copyright &copy; 2023 - Herdiyan Adam Putra &trade;</b></footer>
+        </body>
+
+        </html>
